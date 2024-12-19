8148efad09957bf79db5c8c072cdd96b553fa999
mazhiyuan00014 <3105537600@qq.com>
2024-12-19 15:26:05 +0800

    Football(add the menu to help the users)

--- football/src/Main.java
+++ football/src/Main.java
@@ -4,38 +4,108 @@ import java.util.Scanner;
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
-    public static void main(String[] args) {
+
+    String Forward = "Forward";
+    String Midfield = "Midfield";
+    String Defender = "Defender";
+    String Yes = "Yes";
+    String No = "No";
+    String ControlBall = "Ball Control";
+    String Counterattack = "Counterattack";
+    static int[] Number;
+    static double[] PlayerAbility;
+    static String[] PlayerPosition;
+    static double[] PlayerNumber;
+    Scanner scanner = new Scanner(System.in);
+    private int mainMenu(){
+        System.out.print("""
+            Data Menu
+            ---------
+            1)Set player's ability
+            2)compare
+            3)List the players' ability
+            4)add new players' ability
+            5)Set the tactical of the player
+            6)Set the player as Captain
+            0)Exit
+            ==>>"""
+        );
+        int option = scanner.nextInt();
+        return  option;
+    }
+    private void runMenu() {
+        int option = mainMenu();
+        while (option != 0) {
+            switch (option) {
+                case 1 -> setAbility();
+                case 2 -> compare();
+                case 3 -> list();
+                case 5 -> setTactical();
+                default -> System.out.print("Invalid option entered: " + option);
+
+            }
+            System.out.print("\nPress enter key to continue");
+            scanner.nextLine();
+            scanner.nextLine();
+            option = mainMenu();
+
+        }
+        System.out.print("\n" + "It's over.Thank you for the using");
+        System.exit(0);
+    }
+
+    public  static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
-        System.out.print("Hello and welcome!" + "\n" + "You can calculate the ability of the player and change it in some conditons here" + "\n");
+        System.out.print("Hello and welcome!" + "\n" + "You can calculate the ability of the player and change it in some conditions here" + "\n");
         Scanner scanner = new Scanner(System.in);
-        String Foward = "Foward";
-        String Midfield = "Midfield";
-        String Defender = "Defender";
-        String Yes = "Yes";
-        String No = "No";
-        String ControlBall = "Ball Control";
-        String Counterattack = "Counterattack";
 
-        double[] PlayerAbility;
+
+
+
+
+        Number = new int[10];
         PlayerAbility = new double[20];
-
-        String[] PlayerPosition;
         PlayerPosition = new String[20];
+        Main main = new Main();
+        main.mainMenu();
+        main.runMenu();
 
-        double[] PlayerNumber;
-        System.out.print("How many players'abilities do you want to set up?");
+
+
+
+
+
+
+
+        String in  = scanner.nextLine();
+        System.out.print(in);//To avoid the Bug of Java
+
+
+        System.out.print("\n" + "You can set the player(s) to be Captain(Yes or No):");
+
+
+
+
+
+
+
+    }//end of main
+    public void  setAbility(){
+        System.out.print("How many players' abilities do you want to set up?");
         int numbers = scanner.nextInt();
+        Number[1]= numbers;
         PlayerNumber = new double[numbers];
+
 
         int i = 0;
         while (i < numbers) {
 
-            System.out.print("Please give the position of the palyer ( Foward or Midfield or Defender ):" + "\n");
+            System.out.print("Please give the position of the player ( Forward or Midfield or Defender ):" + "\n");
             String position = scanner.nextLine();
             PlayerPosition[i]= position;
 
-            if (position.equals(Foward)) {
+            if (position.equals(Forward)) {
 
                 System.out.print("Please give the name:");
                 String Name = scanner.nextLine();
@@ -50,7 +120,7 @@ public class Main {
                 System.out.print("Please give the Jumping Ability:");
                 double Jumping = scanner.nextDouble();
                 System.out.print("Please give the OffensiveAwareness Ability:");
-                double OffeensiveAwareness = scanner.nextDouble();
+                double OffensiveAwareness = scanner.nextDouble();
                 System.out.print("Please give the BallContol Ability:");
                 double BallControl = scanner.nextDouble();
                 System.out.print("Please give the Dribbling Ability:");
@@ -65,7 +135,7 @@ public class Main {
                 double BodyContact = scanner.nextDouble();
                 System.out.print("Please give the BodyControl Ability:");
                 double BodyControl = scanner.nextDouble();
-                double ability = calculation1.AbilityCalculate(GroundPassing, AerialPassing, Shooting, Heading, Jumping, OffeensiveAwareness, BallControl, Dribbling, Arc, Speed, Accelerateing, BodyContact, BodyControl);
+                double ability = calculation1.AbilityCalculate(GroundPassing, AerialPassing, Shooting, Heading, Jumping, OffensiveAwareness, BallControl, Dribbling, Arc, Speed, Accelerateing, BodyContact, BodyControl);
                 System.out.print(" The player " + Name + " ability is " + ability);
                 PlayerNumber[i] = ability;
                 PlayerAbility[1]= GroundPassing;
@@ -73,7 +143,7 @@ public class Main {
                 PlayerAbility[3]= Shooting;
                 PlayerAbility[4]= Heading;
                 PlayerAbility[5]= Jumping;
-                PlayerAbility[6]= OffeensiveAwareness;
+                PlayerAbility[6]= OffensiveAwareness;
                 PlayerAbility[7]= BallControl;
                 PlayerAbility[8]= Dribbling;
                 PlayerAbility[9]= Arc;
@@ -188,43 +258,54 @@ public class Main {
                 PlayerAbility[13]= BodyControl;
                 i++;
             }//end of else if
+
         }//end of while
 
-        for (int q = 0; q < numbers; q++) {
+
+
+    }
+
+        public void list(){
+        for (int q = 0; q < Number[1]; q++) {
             int m = q + 1;
             System.out.print("\n" + "The Player" + m + "'s ability is" + PlayerNumber[q]);
-
         }
+
+
+    }
+    public void compare(){
         String h  = scanner.nextLine();
         System.out.print(h);//To avoid the Bug of Java
-
-        if(numbers>1) {
+        if(Number[1]>1) {
             System.out.print("\n" + "Do you want to compare the player's ability?(Yes or No):");
             String opinion = scanner.nextLine();
             if (opinion.equals(Yes)) {
-                System.out.print("Which two players do you want to compare?(Please give the playernumber):");
-                int o = scanner.nextInt();
-                int p = scanner.nextInt();
-                if (PlayerNumber[o - 1] > PlayerNumber[p - 1]) {
-                    System.out.println("Player" + o + "is better" + "\n" + PlayerNumber[o - 1]);
-                } else if (PlayerNumber[o - 1] == PlayerNumber[p - 1]) {
-                    System.out.println("Player" + p + " is better" + "\n" + PlayerNumber[p - 1]);
-                } else if (PlayerNumber[o - 1] < PlayerNumber[p - 1]) {
-                    System.out.println("The two players' ability is equivalent" + "\n" + PlayerNumber[o - 1] + "\n" + PlayerNumber[p - 1]);
-                }
-            } else if (opinion.equals(No)) {
-                System.out.print("All right,this 'Compare'part is over");
+                System.out.print("How many times do you want to compare?:");
+                int Cyclenumber = scanner.nextInt();
+                for (int l = 0; l < Cyclenumber; l++) {
+                    System.out.print("Which two players do you want to compare?(Please give the playernumber):");
+                    int o = scanner.nextInt();
+                    int p = scanner.nextInt();
+                    if (PlayerNumber[o - 1] > PlayerNumber[p - 1]) {
+                        System.out.println("Player" + o + "is better" + "\n" + PlayerNumber[o - 1]);
+                    } else if (PlayerNumber[o - 1] == PlayerNumber[p - 1]) {
+                        System.out.println("Player" + p + " is better" + "\n" + PlayerNumber[p - 1]);
+                    } else if (PlayerNumber[o - 1] < PlayerNumber[p - 1]) {
+                        System.out.println("The two players' ability is equivalent" + "\n" + PlayerNumber[o - 1] + "\n" + PlayerNumber[p - 1]);
+                    } else if (opinion.equals(No)) {
+                        System.out.print("All right,this 'Compare'part is over");
 
+                    }
+                }//end of ofr
             }
         }//end of if
-        else if (numbers<=1){
+        else if (Number[1]<=1){
             System.out.print("\n" + "You only set up one player's ability,so there is no need to compare");
 
         }//end of else if
-        String in  = scanner.nextLine();
-        System.out.print(in);//To avoid the Bug of Java
-
-        System.out.print("\n" + "How many players do you want to give Tactical?"+ "\n" + "(You have set "+ numbers +" playerability)");
+    }
+    public void setTactical(){
+        System.out.print("\n" + "How many players do you want to give Tactical?"+ "\n" + "(You have set "+ Number[1] +" playerability)");
         int numberTactical = scanner.nextInt();
         for(int q =0;q<numberTactical;q++) {
             System.out.print("\n " + "Which player do you want to give the");
@@ -234,7 +315,7 @@ public class Main {
             System.out.print("\n" + "Please give the TacticalAdaptability of the player(1-100):");
             int Tacticalability = scanner.nextInt();
             double AddtionalWeiht = Tacticals.tactical(Tacticalability);
-            if(Tactical.equals(ControlBall)&& PlayerPosition[w-1].equals(Foward)) {
+            if(Tactical.equals(ControlBall)&& PlayerPosition[w-1].equals(Forward)) {
                 double abilitynow = calculation1.AbilityCalculate(PlayerAbility[1]*(1+AddtionalWeiht),PlayerAbility[2]*(1+AddtionalWeiht),PlayerAbility[3],PlayerAbility[4],PlayerAbility[5],PlayerAbility[6],PlayerAbility[7]*(1+AddtionalWeiht),PlayerAbility[8],PlayerAbility[9],PlayerAbility[10],PlayerAbility[11],PlayerAbility[12],PlayerAbility[13]);
                 System.out.print("\n" + "The ability of the player now is " + abilitynow);
             }
@@ -249,7 +330,7 @@ public class Main {
 
 
             }
-            else if(Tactical.equals(Counterattack)&& PlayerPosition[w-1].equals(Foward)) {
+            else if(Tactical.equals(Counterattack)&& PlayerPosition[w-1].equals(Forward)) {
                 double abilitynow = calculation1.AbilityCalculate(PlayerAbility[1],PlayerAbility[2],PlayerAbility[3],PlayerAbility[4],PlayerAbility[5],PlayerAbility[6],PlayerAbility[7],PlayerAbility[8],PlayerAbility[9],PlayerAbility[10]*(1+AddtionalWeiht),PlayerAbility[11]*(1+AddtionalWeiht),PlayerAbility[12],PlayerAbility[13]*(1+AddtionalWeiht));
                 System.out.print("\n" + "The ability of the player now is " + abilitynow);
 
@@ -266,14 +347,8 @@ public class Main {
             }
 
         }//end of for(tactical)
-        System.out.print("\n" + "You can set the player(s) to be Captain(Yes or No):");
 
-        System.out.print("\n" + "It's over.Thank you for the using");
-
-
-
-
-    }//end of main
+    }
 
 
 
